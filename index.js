ab2528223ef277317609a24536a38e225d7cb0fe
Rodolfo Ruiz Pardo <34179410+rodosj89@users.noreply.github.com>
2022-03-17 16:17:25 -0300

    create index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const mongoose = require('mongoose');
-const model = require('./models/user');
-
-CRUD()
-
-async function CRUD () {
-    try{
-        //connection to database
-        const URL = 'mongodb+srv://api-licencia:Zm8wZgaedO3lKaTC@cluster0.zxewk.mongodb.net/licencias?retryWrites=true&w=majority'
-
-        let rta = await mongoose.connect(URL, {});   
-    //    console.log(rta);
-        console.log('Base de datos conectada');
-
-        //create
-        console.log('Create');
-        const user = 
-            ({name: 'Jose', lastname: 'Mei', mail: "mail@gmail.com", priority: 'low', area:"CTO", isAdmin:true},
+const users = [{name: 'Jose', lastname: 'Mei', mail: "mail@gmail.com", priority: 'low', area:"CTO", isAdmin:true},
             {name: 'Ana', lastname: 'Gonzalez', mail: "otro@gmail.com", priority: 'low',area:"COO", isAdmin:false},
-            {name: 'Daniel', lastname: 'Gallo', mail: "nose@gmail.com", priority: 'high',area:"CPO",isAdmin:false})
-        const userSaveModel = model(user);
-        let userSave = await userSaveModel.save();
-        console.log(userSave);
+            {name: 'Daniel', lastname: 'Gallo', mail: "nose@gmail.com", priority: 'high',area:"CPO",isAdmin:false}];
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const app = require('./app');
 
-        //read
-        console.log('Read all');
-        let users = await model.find({});
-        console.log(users);
-    }    
-    catch(error){
-        console.log(`error en CRUD: ${error}`);
-        throw new Error('Error al conectarse a la base de datos');
+const port = process.env.PORT || 3000;
+const hostDB = process.env.HOST_DB || 'mongodb+srv://api-licencia:Zm8wZgaedO3lKaTC@cluster0.zxewk.mongodb.net/licencias?retryWrites=true&w=majority';
+
+// mongoose.set("useFindAndModify", false);
+mongoose.connect(`${hostDB}`, { useNewUrlParser: true}, (err, res) => {
+    if (err)
+        throw err;
+    else {
+        console.log(`La conexion a la base de datos fue exitosa`);
+        app.listen(port, () => {
+            console.log(`Iniciando Servidor...`);
+            console.log(`http://localhost:${port}/api/v1/`);
+        });
+        User.collection.insert(users, function (err, docs) {
+          if (err){ 
+              return console.error(err);
+          } else {
+            console.log("Multiple documents inserted to Collection");
+          }
+        });
     }
-}
+});
