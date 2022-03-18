db86ca2daadac91566a53f31cdea67619569929c
Julian Matta <j.a.matta@hotmail.com>
2022-03-18 10:37:58 -0300

    arreglado date

--- models/application.js
+++ models/application.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 const applicationCollection = 'applications';
 
 const applicationSchema = new mongoose.Schema({
     userId: {type: Schema.ObjectId, ref:"user", require: true},
     vendorId: {type: Schema.ObjectId, ref:"vendor", require: true},
-    create:{type: Date, default: Day.now},
+    create:{type: Date, default: Date.now},
     days:{type: Number, dafault: 30},
     used:{type: Boolean, default: false},
     isStatus: {type: Boolean, default: false}
 })
+
 
 module.exports = mongoose.model(applicationCollection, applicationSchema);
